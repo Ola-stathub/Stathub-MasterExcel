1de9f979ab81e6844d809295eae9f106df4107be
Oladele Bidemi Ajayi <ajayioladeleb@gmail.com>
2026-06-22 02:46:42 +0100

    Increase MAX_JOINS from 10 to 20

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from google.oauth2.service_account import Credentials
 
 MEET_LINK = "https://meet.google.com/yfi-asbp-hog"
-MAX_JOINS = 10
+MAX_JOINS = 20
 ADMIN_PASSWORD = "AdeolaJane"
 GOOGLE_SHEET_NAME = "StatHub Event Database"
 
